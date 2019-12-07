cfa65d27671a9ba18291bf906902ae02e1563b8a
ajacker <ajacker@foxmail.com>
2019-12-07 10:54:18 +0800

    完成 P1303 A*B Problem

---
+++ src/level2/mission19/P1303.java
@@ -0,0 +1,16 @@
+package level2.mission19;
+
+import java.util.Scanner;
+
+/**
+ * P1303 A*B Problem
+ *
+ * @author ajacker
+ * @date 2019/12/7 10:53
+ */
+public class P1303 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(sc.nextBigInteger().multiply(sc.nextBigInteger()));
+    }
+}
