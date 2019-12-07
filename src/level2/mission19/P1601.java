2e6f844af11bc61669900e6a5917f588194a5e17
ajacker <ajacker@foxmail.com>
2019-12-07 10:41:18 +0800

    完成 P1601 A+B Problem（高精）

---
+++ src/level2/mission19/P1601.java
@@ -0,0 +1,17 @@
+package level2.mission19;
+
+
+import java.util.Scanner;
+
+/**
+ * P1601 A+B Problem（高精）
+ *
+ * @author ajacker
+ * @date 2019/12/7 9:59
+ */
+public class P1601 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print(sc.nextBigInteger().add(sc.nextBigInteger()));
+    }
+}
