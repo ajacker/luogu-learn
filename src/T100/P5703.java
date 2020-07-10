1db3c26e11b40a5a89df4ae57e8d54d048b0b343
ajacker <ajacker@foxmail.com>
2020-07-10 19:41:31 +0800

    完成 P5703 苹果采购

---
+++ src/T100/P5703.java
@@ -0,0 +1,18 @@
+package T100;
+
+import java.util.Scanner;
+
+/**
+ * P5703 【深基2.例5】苹果采购
+ *
+ * @author ajacker
+ * @date 2020/7/10 18:30
+ */
+public class P5703 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int each = sc.nextInt();
+        int num = sc.nextInt();
+        System.out.println(each * num);
+    }
+}
